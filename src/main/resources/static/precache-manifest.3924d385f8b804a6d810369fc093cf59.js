49cbbb1721451dca11217ecdaf0139523c64a8e1
Konstantin Hadjiyankov <k.hadjiyankov@student.fontys.nl>
2020-11-05 23:29:56 +0100

    Deploy1

---
+++ src/main/resources/static/precache-manifest.3924d385f8b804a6d810369fc093cf59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81464a4315b8d20792720ee58a1fa664",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e5a5423c5f5696f3a85",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "29d39041141edccbc35f",
+    "url": "/static/css/main.47c9c1b5.chunk.css"
+  },
+  {
+    "revision": "5e5a5423c5f5696f3a85",
+    "url": "/static/js/2.42f1a520.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.42f1a520.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29d39041141edccbc35f",
+    "url": "/static/js/main.52d06bec.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
